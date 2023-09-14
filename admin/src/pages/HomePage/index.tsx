f35ff95903170dc9c6f8cf375d75cf875dfb07cb
Marco Padillo <marco@cynder.io>
2023-09-14 15:28:26 +0800

    feat: plugin ui

--- admin/src/pages/HomePage/index.tsx
+++ admin/src/pages/HomePage/index.tsx
@@ -1,19 +1,30 @@
-/*
- *
- * HomePage
- *
- */
-
 import React from 'react';
-import pluginId from '../../pluginId';
+import { Helmet } from 'react-helmet';
 
-const HomePage = () => {
-  return (
-    <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
-    </div>
-  );
-};
+import {
+	Box,
+	Button,
+	ContentLayout,
+	GridLayout,
+	HeaderLayout,
+	Main,
+	TextInput,
+} from '@strapi/design-system';
+
+const HomePage = () => (
+	<Main>
+		<Helmet title="Shopify" />
+
+		<HeaderLayout primaryAction={<Button>Save</Button>} title="Shopify" />
+		<ContentLayout>
+			<GridLayout>
+				<Box>
+					<TextInput label="Access Token" name="accessToken" />
+				</Box>
+				<Box />
+			</GridLayout>
+		</ContentLayout>
+	</Main>
+);
 
 export default HomePage;
