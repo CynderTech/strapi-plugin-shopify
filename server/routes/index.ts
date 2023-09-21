f74943d29fbfd4724e8fa48709e2cbcaa7fecec3
Marco Padillo <marco@cynder.io>
2023-09-21 15:57:48 +0800

    fix: require authorization for getSettings

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,19 +1,18 @@
 export default [
-  {
-    method: 'GET',
-    path: '/settings',
-    handler: 'settings.getSettings',
-    config: {
-      policies: [],
-      auth: false,
-    },
-  },
-  {
-    method: 'PUT',
-    path: '/settings',
-    handler: 'settings.updateSettings',
-    config: {
-      policies: [],
-    },
-  },
+	{
+		method: 'GET',
+		path: '/settings',
+		handler: 'settings.getSettings',
+		config: {
+			policies: [],
+		},
+	},
+	{
+		method: 'PUT',
+		path: '/settings',
+		handler: 'settings.updateSettings',
+		config: {
+			policies: [],
+		},
+	},
 ];
